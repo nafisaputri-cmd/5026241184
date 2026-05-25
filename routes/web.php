b3c199667786d2cff4a04507ceeba54984c4b9c8
nafisaputri <nafisaputriazzahro05@gmail.com>
2026-05-25 12:23:58 +0700

    Pertemuan13

--- routes/web.php
+++ routes/web.php
@@ -6,71 +6,50 @@ use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\pegawaiDBController;
 use App\Http\Controllers\BlogController;
 
-
-
+// GENERAL
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('halo', function () {
-	return "<h1>Halo, Selamat datang</h1> di tutorial laravel <b>www.malasngoding.com</b>";
+Route::get('/halo', function () {
+    return "<h1>Halo, Selamat datang</h1> di tutorial laravel <b>www.malasngoding.com</b>";
 });
 
-Route::get('blog', function () {
-	return view('blog');
-});
+// DOSEN
+Route::get('/dosen', [DosenController::class, 'index']);
+Route::get('/biodata', [DosenController::class, 'biodata']);
 
-Route::get('dosen', [DosenController::class, 'index']);
-
-Route::get('biodata', [DosenController::class, 'biodata']);
-
+// PEGAWAI
+Route::get('/pegawai', [pegawaiDBController::class, 'index']);
 Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
-Route::get('/formulir', [PegawaiController::class,'formulir']);
-Route::post('/formulir/proses', [PegawaiController::class,'proses']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
 
-// route blog
+
+// BLOG
 Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
 
-//crud
-Route::get('/pegawai', [pegawaiDBController::class,'index']);
 
-// ✅ BENAR
-Route::get('nrp', function () {
-    return view('5026241184');
-});
+// PERTEMUAN / LATIHAN
+Route::get('/nrp', function () { return view('5026241184'); });
+Route::get('/menu', function () { return view('menu'); });
+Route::get('/master', function () { return view('master'); });
+
+Route::get('/pertemuan1-intro', function () { return view('pertemuan1-intro'); });
+Route::get('/pertemuan2-news', function () { return view('pertemuan2-news'); });
+Route::get('/pertemuan3-responsive', function () { return view('pertemuan3-responsive'); });
+Route::get('/pertemuan3-nyamnyam', function () { return view('pertemuan3-nyamnyam'); });
+Route::get('/pertemuan4-5026241184', function () { return view('pertemuan4-5026241184'); });
+Route::get('/pertemuan5-dell', function () { return view('pertemuan5-dell'); });
+Route::get('/pertemuan5-simply', function () { return view('pertemuan5-simply'); });
 
 
-Route::get('menu', function () {
-    return view('menu');
-});
-
-Route::get('pertemuan1-intro', function () {
-    return view('pertemuan1-intro');
-});
-
-Route::get('pertemuan2-news', function () {
-    return view('pertemuan2-news');
-});
-
-Route::get('pertemuan3-responsive', function () {
-    return view('pertemuan3-responsive');
-});
-
-Route::get('pertemuan3-nyamnyam', function () {
-    return view('pertemuan3-nyamnyam');
-});
-
-Route::get('pertemuan4-5026241184', function () {
-    return view('pertemuan4-5026241184');
-});
-
-Route::get('pertemuan5-dell', function () {
-    return view('pertemuan5-dell');
-});
-
-Route::get('pertemuan5-simply', function () {
-    return view('pertemuan5-simply');
-});
-
+Route::get('/pegawai', [pegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [pegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [pegawaiDBController::class, 'store']);
+Route::get('/pegawaiedit{id}', [pegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update', [pegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus{id}', [pegawaiDBController::class, 'hapus']);
+Route::get('/pegawai/cari', [pegawaiDBController::class,'cari']);
